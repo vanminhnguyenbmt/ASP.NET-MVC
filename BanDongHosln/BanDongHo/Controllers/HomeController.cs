5a1a149743e6a2a0132f59c536b47077e473359d
tuvanthuan1996@gmail.com <tuvanthuan1996@gmail.com>
2018-04-06 22:13:53 +0700

    Config Project

---
+++ BanDongHosln/BanDongHo/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanDongHo.Models.Service;
+using BanDongHo.Models.ViewModel;
+
+namespace BanDongHo.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRegisterSercive registerservice;
+        public HomeController()
+        {
+            
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            Register register = new Register();
+            ViewBag.MessageRegister = "";
+            return View(register);
+        }
+        [HttpPost]
+        public ActionResult Register(Register register)
+        {
+            ViewBag.MessageRegister = "";
+            // Kiểm tra dữ liệu
+            registerservice = new RegisterService();
+            if (registerservice.isExistAccount(register.Account))
+            {
+                register.Account = "";
+                ViewBag.MessageRegister += "Tài khoản đã tồn tại!";
+                return View(register);
+            }
+            if (!registerservice.isPasswordAccount(register.Password))
+            {
+                register.Password = "";
+                ViewBag.MessageRegister += "Mật khẩu sai định dạng!";
+                return View(register);
+            }
+            return View(register);
+        }
+
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ BanDongHosln/BanDongHo/Models/Service/RegisterService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BanDongHo.Domain.DataContext;
+using System.Text.RegularExpressions;
+using BanDongHo.Models.ViewModel;
+
+namespace BanDongHo.Models.Service
+{
+    public class RegisterService : IRegisterSercive
+    {
+        const string ID_DEFAULT = "KH00001";
+        public bool isExistAccount(string account)
+        {
+                // get TK => tk.TenDN == account
+                // if tk ==null return true ? false;
+                BANDONGHOEntities db = new BANDONGHOEntities();
+                TAIKHOAN taikhoan = (from tk in db.TAIKHOANs
+                                     where tk.TENDN.Equals(account)
+                                     select tk).SingleOrDefault();
+                if (taikhoan != null)
+                {
+                    return true;
+                }
+                return false;
+        }
+
+        public bool isPasswordAccount(string password)
+        {
+            return Regex.IsMatch(password, @"\w");
+        }
+
+        public bool RegisterAccount(Register register)
+        {
+            string makh = "";
+            BANDONGHOEntities db = new BANDONGHOEntities();
+            /* Thêm Tài khoản */
+
+            /* Thêm khách hàng */
+            #region Tạo mã KH
+            // lấy mã khách hàng lớn nhất hiện tại
+            KHACHHANG kh = (from k in db.KHACHHANGs
+                            orderby k.MAKH descending
+                            select k).SingleOrDefault();
+            if (kh == null)
+            {
+                makh = ID_DEFAULT;
+            }
+            else
+            {
+                //cắt 2 ký tự đầu
+                int numberId;
+                if (!Int32.TryParse(makh.Substring(2), out numberId))
+                {
+                    makh = ID_DEFAULT;
+                }
+                else
+                {
+                    numberId++;
+                    string newId = "KH";
+                    for (int i = 0; i < 5 - numberId.ToString().Length; i++)
+                    {
+                        newId += "0";
+                    }
+                    newId += numberId.ToString();
+                    makh = newId;
+                }
+            }
+            #endregion
+            // tạo mới khách hàng
+            KHACHHANG customer = new KHACHHANG { MAKH = makh, HOTEN = register.FirstName + register.LastName, DIACHI = "", SDT = register.Phone, GIOITINH = register.Sex };
+            // Thêm khách hàng và db
+            db.KHACHHANGs.Add(customer);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
