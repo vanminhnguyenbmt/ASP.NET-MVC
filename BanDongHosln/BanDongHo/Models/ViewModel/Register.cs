4569494997529423a4deead028b2498d0b3a05e8
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-04-07 21:24:38 +0700

    reup

--- BanDongHosln/BanDongHo/Models/ViewModel/Register.cs
+++ BanDongHosln/BanDongHo/Models/ViewModel/Register.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace BanDongHo.Models.ViewModel
 {
-    public class RegisterViewModel
+    public class Register
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Address { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
         public string Phone { get; set; }
         public string Sex { get; set; }
         public string Account { get; set; }
         public string Password { get; set; }
+
     }
 }
