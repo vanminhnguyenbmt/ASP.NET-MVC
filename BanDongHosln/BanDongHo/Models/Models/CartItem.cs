aa8fc1101f390a03306ae2a476b8c5e9e663fba9
tuvanthuan1996@gmail.com <tuvanthuan1996@gmail.com>
2018-04-14 11:09:06 +0700

    Xong Gio hang

---
+++ BanDongHosln/BanDongHo/Models/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BanDongHo.Domain.DataContext;
+
+namespace BanDongHo.Models.Models
+{
+    public class CartItem
+    {
+        public SANPHAM Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ BanDongHosln/BanDongHo/Models/ViewModel/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BanDongHo.Domain;
+using BanDongHo.Models.Models;
+
+namespace BanDongHo.Models.ViewModel
+{
+    public class CartViewModel
+    {
+        public Cart CustomCart { get; set; }
+    }
+}
