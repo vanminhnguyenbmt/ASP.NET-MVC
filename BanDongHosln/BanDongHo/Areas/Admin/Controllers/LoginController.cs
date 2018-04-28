2e5e169fa067d6fc93ab0169ca3006803ca7827f
ochutgio <ochutgio@gmail.com>
2018-04-29 00:54:55 +0700

    Create Area Admin

---
+++ BanDongHosln/BanDongHo/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanDongHo.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
